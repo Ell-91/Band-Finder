8fec2830f3a8c08da29a55debf6ef291d5efd3eb
Ell-91 <cdsherman@ucdavis.edu>
2022-04-20 16:28:07 -0700

    updated add project page, not static anymore

--- bandfinder/projects/forms.py
+++ bandfinder/projects/forms.py
@@ -1,7 +1,18 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    # To make changes to title method 
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'}) 
